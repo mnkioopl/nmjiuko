232143631cbc66c9f1e725002043413652868f92
mnkioopl <tyuji@atomicmail.io>
2026-01-28 16:38:59 +1200

    Update test_multiprocessing_forkserver.py

--- test_multiprocessing_forkserver.py
+++ test_multiprocessing_forkserver.py
@@ -9,6 +9,7 @@ from test import support
 
 if support.PGO:
     raise unittest.SkipTest("test is not helpful for PGO")
+    
 
 test._test_multiprocessing.install_tests_in_module_dict(globals(), 'forkserver')
 
